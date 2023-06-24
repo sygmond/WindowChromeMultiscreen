28bd255863831037b5bd815adca618b3c7b970b0
Sygmond <sygmond@hotmail.com>
2023-06-24 23:15:13 +0200

    Added Window Chrome with Titlebar Buttons and Styles

---
+++ src/WindowChromeMultiscreen.UI/Converters/DoubleToGridLenghtConverter.cs
@@ -0,0 +1,20 @@
+namespace WindowChromeMultiscreen.UI.Converters;
+
+public class DoubleToGridLenghtConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is null)
+            throw new ArgumentException($"Can't convert null to {nameof(GridLength)}.", nameof(value));
+
+        if (value is not double inputValue)
+            throw new ArgumentException($"Input value must be of type double.", nameof(value));
+
+        return new GridLength(inputValue);
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
--- src/WindowChromeMultiscreen.UI/MainWindow.xaml.cs
+++ src/WindowChromeMultiscreen.UI/MainWindow.xaml.cs
@@ -8,6 +8,9 @@
         public MainWindow()
         {
             InitializeComponent();
+
+            RefreshMaximizeRestoreButton();
+            MouseLeftButtonDown += (_, _) => { DragMove(); };
         }
 
         protected override void OnSourceInitialized(EventArgs e)
@@ -15,5 +18,34 @@
             base.OnSourceInitialized(e);
             ((HwndSource)PresentationSource.FromVisual(this))?.AddHook(NativeMethods.HookProc);
         }
+
+        #region Minimize, Maximize, Restore and Close buttons
+
+        private void OnCloseButtonClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void OnMinimizeButtonClick(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void OnMaximizeRestoreButtonClick(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+        }
+
+        private void RefreshMaximizeRestoreButton()
+        {
+            var isMaximized = WindowState == WindowState.Maximized;
+
+            MaximizeButton.Visibility = isMaximized ? Visibility.Collapsed : Visibility.Visible;
+            RestoreButton.Visibility = isMaximized ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        #endregion // End of: Minimize, Maximize, Restore and Close buttons
+
+        private void Window_StateChanged(object sender, EventArgs e) => RefreshMaximizeRestoreButton();
     }
 }
